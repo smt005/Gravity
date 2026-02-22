8bcc0788e26179c68f86d485dfe6fd7e528c22cb
HomePC\Ko <intelligencecells@gmail.com>
2026-02-22 15:52:04 +0300

    ImGuiWindow обновление

--- Source/Engine/Source/ImGuiWindow/ImGuiWindow.h
+++ Source/Engine/Source/ImGuiWindow/ImGuiWindow.h
@@ -1,4 +1,4 @@
-﻿// ◦ Xyz ◦
+// ◦ Xyz ◦
 #pragma once
 
 #include <memory>
@@ -14,38 +14,49 @@ namespace Engine
 		using Ptr = std::shared_ptr<ImGuiWindow>;
 		using Wptr = std::weak_ptr<ImGuiWindow>;
 
-		ImGuiWindow() = delete;
-		ImGuiWindow(std::string_view name);
+	public:
+		ImGuiWindow();
+		ImGuiWindow(const std::string& name);
 		virtual ~ImGuiWindow() = default;
 
+		ImGuiWindow(const ImGuiWindow&) = delete;
+		ImGuiWindow(ImGuiWindow&&) = delete;
+		void operator = (const ImGuiWindow&) = delete;
+		void operator = (ImGuiWindow&&) = delete;
+
+		virtual void Render() = 0;
 		virtual void Update(double dTime) {};
-		virtual void Render() {};
 		virtual void Resize() {}
 		virtual void OnOpen() {};
 		virtual void OnClose() {};
+		virtual void SetFullScreen(bool state);
 
 		const std::string& GetName() const;
+		const std::string& GetTitle() const;
+		void SetTitle(std::string_view title);
 		bool IsVisible() const;
 		void SetVisible(bool state);
 		bool IsFullScreen() const;
-		void SetFullScreen(bool state);
 		void Close();
 
 	private:
-		bool _visible;
-		bool _fullScreen;
+		bool _visible = true;
+		bool _fullScreen = false;
+		int _flags = 1 << 5; // ImGuiWindowFlags::ImGuiWindowFlags_NoCollapse
 		std::string _name;
+		std::string _title;
 
 	public:
-		template <typename T>
-		static std::shared_ptr<T> MakeWindow(std::string_view name) {
-			std::shared_ptr<T> newWindow = std::make_shared<T>(name);
-			const auto itPair = _windows.emplace(std::string(name), newWindow);
-		
+		template <typename T, typename ...Args>
+		static std::shared_ptr<T> MakeWindow(Args&&... args) {
+			std::shared_ptr<T> newWindow = std::make_shared<T>(std::forward<Args>(args)...);
+			const auto itPair = _windows.emplace(newWindow.get()->GetName(), newWindow);
+
 			if (!itPair.second) {
-				throw(TO_STRING("[ImGuiWindow::Ptr Make] duplicate name: {} class: {}/", name, typeid(T).name()));
+				throw(TO_STRING("[ImGuiWindow::Ptr Make] duplicate name: {} class: {}.", "name", typeid(T).name()));
 			}
 
+			newWindow->OnOpen();
 			return newWindow;
 		}
 
