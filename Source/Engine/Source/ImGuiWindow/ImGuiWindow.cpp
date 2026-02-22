8d3aad4f24f9ad15a4278104a2344f0751567bd2
HomePC\Ko <intelligencecells@gmail.com>
2026-02-22 03:46:10 +0300

    Окна ImGui.

---
+++ Source/Engine/Source/ImGuiWindow/ImGuiWindow.cpp
@@ -0,0 +1,138 @@
+﻿// ◦ Xyz ◦
+#include "ImGuiWindow.h"
+#include <imgui.h>
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <Screen.h>
+
+using namespace Engine;
+
+ImGuiWindow::ImGuiWindow(std::string_view name)
+	: _visible(true)
+    , _fullScreen(false)
+    , _name(name)
+{
+    OnOpen();
+}
+
+const std::string& ImGuiWindow::GetName() const
+{
+	return _name;
+}
+
+bool ImGuiWindow::IsVisible() const
+{
+    return _visible;
+}
+
+void ImGuiWindow::SetVisible(bool state)
+{
+    _visible = state;
+}
+
+bool ImGuiWindow::IsFullScreen() const
+{
+    return _fullScreen;
+}
+
+void ImGuiWindow::SetFullScreen(bool state)
+{
+    _fullScreen = state;
+    Resize();
+}
+
+void ImGuiWindow::Close()
+{
+	CloseWindow(GetName());
+}
+
+bool ImGuiWindow::Init(void* window)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsLight();
+
+	ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)window, true);
+	return ImGui_ImplOpenGL3_Init();
+}
+
+void ImGuiWindow::Cleanup()
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+}
+
+void ImGuiWindow::RenderWindows()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    for (const auto& [name, window] : _windows) {
+        if (window && window->IsVisible()) {
+            if (window->IsFullScreen()) {
+                ImGui::SetNextWindowPos(ImVec2(0, 0));
+                ImGui::SetNextWindowSize(ImVec2(static_cast<float>(ScreenParams::width), static_cast<float>(ScreenParams::height)));
+                ImGui::Begin(window->GetName().c_str(), nullptr,
+                    ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+                    ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse |
+                    ImGuiWindowFlags_NoBringToFrontOnFocus);
+            }
+            else {
+                ImGui::Begin(window->GetName().c_str());
+            }
+            window->Render();
+            ImGui::End();
+        }
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    for (auto& window : _windowToClose) {
+        window->OnClose();
+    }
+    _windowToClose.clear();
+}
+
+void ImGuiWindow::CloseWindow(const std::string& name)
+{
+    const auto it = _windows.find(name);
+
+    if (it != _windows.end()) {
+        _windowToClose.emplace_back(std::move(it->second));
+        _windows.erase(it);
+    }
+}
+
+void ImGuiWindow::ResizeWindows()
+{
+    for (const auto& [name, window] : _windows) {
+        window->Resize();
+    }
+}
+
+void ImGuiWindow::UpdateWindows(double dTime)
+{
+    for (const auto& [name, window] : _windows) {
+        window->Update(dTime);
+    }
+}
+
+bool ImGuiWindow::ExistWindow(const std::string& name)
+{
+    return _windows.contains(name);
+}
+
+const ImGuiWindow::Ptr& ImGuiWindow::Get(const std::string name)
+{
+    const auto it = _windows.find(name);
+    if (it == _windows.end()) {
+        throw(TO_STRING("ImGuiWindow::Get window by name: {} no founded.", name));
+    }
+    return it->second;
+}
