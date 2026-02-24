208c66fb7d1ca9d751a5d03cf62e502456809ba9
HomePC\Ko <intelligencecells@gmail.com>
2026-02-25 01:36:55 +0300

    Отображение glm в логах.

--- Source/Program/Temp/LogSpecification.h
+++ Source/Program/Temp/LogSpecification.h
@@ -2,367 +2,38 @@
 
 #include <iostream>
 #include <sstream>
-#include <initializer_list>
-#include <array>
-#include <vector>
-#include <list>
-#include <forward_list>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <exception>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 namespace mystd
 {
-	std::ostream& operator << (std::ostream& os, const std::exception& exc)
+
+	std::ostream& operator << (std::ostream& os, const glm::vec4& vec)
 	{
-		os << "EXCEPTION: " << typeid(exc).name() << ": " << exc.what();
+		os << '[' << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << ']';
 		return os;
 	}
 
-	std::ostream& operator << (std::ostream& os, std::byte byte)
+	std::ostream& operator << (std::ostream& os, const glm::vec3& vec)
 	{
-		os << static_cast<unsigned char>(byte);
+		os << '[' << vec.x << ", " << vec.y << ", " << vec.z << ']';
 		return os;
 	}
 
-	// [value]
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::initializer_list<T>& container)
+	std::ostream& operator << (std::ostream& os, const glm::vec2& vec)
 	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
+		os << '[' << vec.x << ", " << vec.y << ']';
 		return os;
 	}
 
-	/*template <typename T, std::size_t Count>
-	std::ostream& operator << (std::ostream& os, const std::array<T, Count>& container)
+	std::ostream& operator << (std::ostream& os, const glm::mat4x4& vec)
 	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-
-	}*/
-
-	template <typename T>
-	std::ostream& operator << (std::ostream &os, const std::vector<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item: container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
+		os << "\n|" << vec[0][0] << ",\t" << vec[0][1] << ",\t" << vec[0][2] << ",\t" << vec[0][3] << '|';
+		os << "\n|" << vec[1][0] << ",\t" << vec[1][1] << ",\t" << vec[1][2] << ",\t" << vec[1][3] << '|';
+		os << "\n|" << vec[2][0] << ",\t" << vec[2][1] << ",\t" << vec[2][2] << ",\t" << vec[2][3] << '|';
+		os << "\n|" << vec[3][0] << ",\t" << vec[3][1] << ",\t" << vec[3][2] << ",\t" << vec[3][3] << '|';
 		return os;
 	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream &os, const std::list<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item: container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::forward_list<T>& container)
-	{
-		const size_t size = distance(container.begin(), container.end());
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::set<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::multiset<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::unordered_set<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::unordered_multiset<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::deque<T>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : container) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::queue<T>& container)
-	{
-		auto copyContainer = container;
-
-		const size_t size = copyContainer.size();
-		size_t number = 0;
-		os << '[';
-		while (!copyContainer.empty()) {
-			os << copyContainer.front();
-			copyContainer.pop();
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::stack<T>& container)
-	{
-		auto copyContainer = container;
-
-		const size_t size = copyContainer.size();
-		std::vector<T> tempContainer(size);
-
-		int i = size - 1;
-		while (!copyContainer.empty()) {
-			tempContainer[i--] = copyContainer.top();
-			copyContainer.pop();
-		}
-
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : tempContainer) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::priority_queue<T>& container)
-	{
-		auto copyContainer = container;
-
-		const size_t size = copyContainer.size();
-		std::vector<T> tempContainer(size);
-
-		int i = size - 1;
-		while (!copyContainer.empty()) {
-			tempContainer[i--] = copyContainer.top();
-			copyContainer.pop();
-		}
-
-		size_t number = 0;
-		os << '[';
-		for (auto&& item : tempContainer) {
-			os << item;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	// [key, value]
-	template <typename Tkey, typename Tvalue>
-	std::ostream& operator << (std::ostream& os, const std::map<Tkey, Tvalue>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& [key, value] : container) {
-			os << key << ':' << value;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename Tkey, typename Tvalue>
-	std::ostream& operator << (std::ostream& os, const std::multimap<Tkey, Tvalue>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& [key, value] : container) {
-			os << key << ':' << value;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename Tkey, typename Tvalue>
-	std::ostream& operator << (std::ostream& os, const std::unordered_map<Tkey, Tvalue>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& [key, value] : container) {
-			os << key << ':' << value;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename Tkey, typename Tvalue>
-	std::ostream& operator << (std::ostream& os, const std::unordered_multimap<Tkey, Tvalue>& container)
-	{
-		const size_t size = container.size();
-		size_t number = 0;
-		os << '[';
-		for (auto&& [key, value] : container) {
-			os << key << ':' << value;
-			if (++number != size) {
-				os << ',';
-			}
-		}
-		os << ']';
-		return os;
-	}
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::shared_ptr<T>& sPtr)
-	{
-		if (!sPtr) {
-			os << "null";
-			return os;
-		}
-		os << *sPtr;
-		return os;
-	};
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::weak_ptr<T>& wPtr)
-	{
-		if (wPtr.expired()) {
-			os << "null";
-			return os;
-		}
-		std::shared_ptr<T> sPtr = wPtr.lock();
-		if (!sPtr) {
-			os << "null";
-			return os;
-		}
-		os << *sPtr;
-		return os;
-	};
-
-	template <typename T>
-	std::ostream& operator << (std::ostream& os, const std::unique_ptr<T>& uPtr)
-	{
-		if (!uPtr) {
-			os << "null";
-			return os;
-		}
-		os << *uPtr;
-		return os;
-	};
 }
