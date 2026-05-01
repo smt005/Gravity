abc6d2513bc7fa36df196a6f02d563ca6c2f5d95
HomePC\Ko <intelligencecells@gmail.com>
2026-05-01 23:37:02 +0300

    Русские буквы в консоле.

--- Source/Engine/Platforms/Win/main.cpp
+++ Source/Engine/Platforms/Win/main.cpp
@@ -1,9 +1,12 @@
 // ◦ Xyz ◦
 
+#include <windows.h>
 #include "../Source/Core.h"
 
 int main(int argc, char** argv)
 {
+	SetConsoleOutputCP(CP_UTF8);
+
 	const std::string_view params(*argv);
 	return Engine::Core::Execution(params);
 }
